c4eb8487c8308a74a117d18567ec63f059656bca
Andrew Snead <andrewcsnead@gmail.com>
2015-02-01 22:20:19 +0000

    mockDB further tests added

--- test/mockDBTest.js
+++ test/mockDBTest.js
@@ -19,4 +19,30 @@ describe('#Mock Database', function() {
     expect(db.returnItem('1')).to.be.above(startStock);
     done();
   });
+
+  it('can take an item from the database', function(done) {
+    expect(db.takeItem('1')).to.be.ok;
+    done();
+  });
+
+  it('reduces the stock of item when taking from database', function(done) {
+    var startStock = db.db['1'].stock;
+    db.takeItem('1');
+    expect(db.db['1'].stock).to.be.below(startStock);
+    done();
+  });
+
+  it('can return all items from database', function(done) {
+    expect(db.allItems().length).to.eql(Object.keys(db.db).length);
+    done()
+  });
+
+  it('can return a filtered list of items', function(done) {
+    var filteredList = db.filteredItems("Men's Footwear");
+    expect(filteredList[0].category).to.eql("Men's Footwear");
+    expect(filteredList.length).to.be.below(Object.keys(db.db).length);
+    done();
+  });
+
+
 });
